be6c74b3bcb46f1a163c12ee5a9ad06e04b5ce86
alireza <comolestyrusistantaneos@gmail.com>
2023-07-18 22:38:57 +0330

    d

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,15 +1,11 @@
 import { Main } from "./components/main";
-import { getDictionary } from "../../get-dictionary";
 import { Locale } from "../../i18n-config";
-import LocaleSwitcher from "./components/locale-switcher";
 
 export default async function IndexPage({
   params: { lang },
 }: {
   params: { lang: Locale };
 }) {
-  const dictionary = await getDictionary(lang);
-
   return (
     <div className="w-full flex flex-col justify-center items-center">
       <Main />
